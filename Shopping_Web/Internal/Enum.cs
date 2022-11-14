1c00b1ad94c09156b1597f00acc5ab2c991a0572
morga.yeh <morga.yeh@hy-tech.com.tw>
2022-11-14 10:08:50 +0800

    [DEV] Mor-常數/列舉添加

---
+++ Shopping_Web/Internal/Enum.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.Runtime.Serialization;
+
+namespace Shopping_Web.Internal
+{
+    /// <summary>
+    /// 轉換字典
+    /// </summary>
+    public enum ConvertDic
+    {
+        /// <summary>
+        /// Produce物件
+        /// </summary>
+        [EnumMember, Description("Produce")]
+        Produce,
+
+        /// <summary>
+        /// 非定義
+        /// </summary>
+        [EnumMember, Description("Unmean")]
+        Unmean,
+    }
+}
